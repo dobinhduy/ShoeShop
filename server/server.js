c85cb1cb4fc548eeeacc79101831c351c1815509
dobinhduy <dobinhduy23022000@gmail.com>
2022-05-05 17:08:33 +0700

    manage error

--- server/server.js
+++ server/server.js
@@ -1,17 +1,23 @@
-import express, { application } from "express";
+import express from "express";
 import dotenv from "dotenv";
 import connectDatabase from "./config/MongoDB.js"
 import ImportData from "../server/DataImport.js";
+import productRouter from  "./Routers/ProductRouters.js"
+import {errorHandler,notFound} from "./Middleware/Errors.js"
+
 
 dotenv.config();
 connectDatabase();
 const app = express();
 
 app.use("/api/import", ImportData);
+app.use("/api/products", productRouter);
 
-app.get("/",(req, res)=>{
-    res.send("Api is running....");
-});
+//Error Handler
+app.use(notFound);
+app.use(errorHandler);
+
+
 const PORT =process.env.PORT || 1000;
 app.listen(PORT,console.log(`server is running.... ${PORT}`));
 
